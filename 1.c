9495fdbcc9a9d256d4f5fee3ad50af40d998b608
2semClab <158600040+2semClab@users.noreply.github.com>
2024-03-04 12:35:40 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+int row , col, sum=0;
+printf("Enter the row and column size of the matrix : ");
+scanf("%d%d", &row, &col);
+printf("Enter the elements of the matrix of order : %d*%d\n",row,col);
+int a[row][col];
+for(int i=0;i<row;i++){
+	for(int j=0;j<col;j++){
+		scanf("%d",&a[i][j]); 
+	}
+}
+for(int i=0; i<row; i++){
+	for(int j=0; j<col; j++){
+		sum += a[i][j];
+	}
+}                         
+printf("Sum of the elemnts of the matrix : %d\n",sum);
+return 0;
+}
